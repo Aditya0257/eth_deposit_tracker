56a1ab999d860abab0931ff4c9e137e8972182ec
Aditya0257 <adityasinghthakur.0257@gmail.com>
2024-09-10 03:10:57 +0530

    init commit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import path from "path";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+
+app.post("/txntracker", (req, res) => {
+  notificationReceived(req);
+  res.status(200).end();
+});
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+const server = createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("Client connected");
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
+function notificationReceived(req) {
+  console.log("Notification received!");
+  io.emit("notification", JSON.stringify(req.body));
+}
+
+server.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
