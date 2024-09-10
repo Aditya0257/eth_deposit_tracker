9e8ce42e106cbb50b8565badaf4b67068f77d0e8
Aditya0257 <adityasinghthakur.0257@gmail.com>
2024-09-10 19:48:52 +0530

    port changed

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const { Interface } = require("@ethersproject/abi");
 const fs = require("fs");
 
 const app = express();
-const port = 5000;
+const port = 3000;
 
 const BEACON_DEPOSIT_CONTRACT = "0x00000000219ab540356cBB839Cbe05303d7705Fa";
 
